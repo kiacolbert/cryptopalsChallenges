8a9de438998effc4c1d0270e317702861c26ddf1
kia <dont@email.com>
2021-09-04 06:35:59 -0400

    put getBase64 into func

--- set_one/main.go
+++ set_one/main.go
@@ -10,15 +10,8 @@ import (
 func main() {
 	hexString := "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
 	baseSixFourString := "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
-	
-	hexBytes, err := hex.DecodeString(hexString) // decode string into hex byte array
 
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	sixFourString := base64.RawStdEncoding.EncodeToString(hexBytes) // encode hex bytes to base 64 string
-	
+	sixFourString := GetBase64StringFromHexString(hexString)
 	if sixFourString != baseSixFourString {
 		fmt.Printf("incorrect")
 	}
@@ -27,6 +20,12 @@ func main() {
 	ans := getXor(hexBytes, hexBytes)
 	fmt.Println("ans", ans)
 
+func GetBase64StringFromHexString(hexString string) string{
+	hexBytes, err := hex.DecodeString(hexString) // decode string into hex byte array
+	if err != nil {
+		fmt.Println(err)
+	}
+	return base64.RawStdEncoding.EncodeToString(hexBytes) // encode hex bytes to base 64 string
 }
 
 // Write a function that takes two equal-length buffers and produces their XOR combination.
